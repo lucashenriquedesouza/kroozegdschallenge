bdf49a46dd4090cd136488b6561bb109a3cf2109
Lucas Henrique de Souza <lucas.souza@iacit.com.br>
2019-08-02 18:24:15 -0300

    Web

---
+++ Krooze.EntranceTest.Web/Controllers/TestsController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using Krooze.EntranceTest.WriteHere.Structure.Model;
+using Krooze.EntranceTest.WriteHere.Tests.LogicTests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Krooze.EntranceTest.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestsController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet("XML")]
+        public ActionResult<CruiseDTO> TranslateXML()
+        {
+            return new XMLTest().TranslateXML();
+        }
+
+        [HttpPost("OtherTaxes")]
+        public decimal? OtherTaxes(CruiseDTO cruise)
+        {
+            return new SimpleLogicTest().GetOtherTaxes(cruise);
+        }
+
+        [HttpPost("IsThereDiscount")]
+        public bool? IsThereDiscount(CruiseDTO cruise)
+        {
+            return new SimpleLogicTest().IsThereDiscount(cruise);
+        }
+
+        [HttpGet("GetInstallments/{fullPrice}")]
+        public int? GetInstallments(decimal fullPrice)
+        {
+            return  new SimpleLogicTest().GetInstallments(fullPrice);
+        }
+
+    }
+
+}
